ee6aaafae5d851dffc406db0dd72479037ddf277
dylanfisher <hi@dylanfisher.com>
2021-05-27 14:27:56 -0400

    Show a video's Vimeo thumbnail preview in the edit form

--- app/models/concerns/forest/vimeo/media_item_extension.rb
+++ app/models/concerns/forest/vimeo/media_item_extension.rb
@@ -19,12 +19,34 @@ module Forest::Vimeo
       vimeo_metadata['link']
     end
 
+    def vimeo_video_width
+      vimeo_metadata['width']
+    end
+
+    def vimeo_video_height
+      vimeo_metadata['height']
+    end
+
     def vimeo_video_upload_status
       vimeo_metadata.dig('upload', 'status')
     end
 
     def vimeo_video_transcode_status
       vimeo_metadata.dig('transcode', 'status')
+    end
+
+    def vimeo_video_thumbnails
+      vimeo_metadata.dig('pictures', 'sizes').to_a
+    end
+
+    def vimeo_video_thumbnail
+      return [] unless vimeo_video_thumbnails.size > 1
+
+      largest_thumbnail = vimeo_video_thumbnails.find do |p|
+        p['width'] == vimeo_video_width && p['height'] == vimeo_video_height
+      end.presence
+      largest_thumbnail = vimeo_video_thumbnails.last if largest_thumbnail.blank?
+      largest_thumbnail.try(:[], 'link')
     end
 
     private
